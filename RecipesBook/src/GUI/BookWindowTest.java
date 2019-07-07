e0c0fdafebc8827fbf27c033fd583077188725fe
FabienManson <fabien.manson@free.fr>
2019-07-07 15:16:27 +0200

    added main window user screen resolution scaling

--- RecipesBook/src/GUI/BookWindowTest.java
+++ RecipesBook/src/GUI/BookWindowTest.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,6 +10,7 @@ import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -23,16 +25,23 @@ public class BookWindowTest extends Application {
 			v.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
 			//v.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, null, null)));
 			
-			Scene scene = new Scene(v, 352, 505);
+			Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+						
+			Image img = new Image("GUI/CookingBookScantitreSD.jpg", screen.getHeight()/2, 0, true, true);
+			ImageView imgView = new ImageView(img);
+			System.out.println("Image resize ["+img.getHeight()+", "+img.getWidth()+"]");
+			
+			Scene scene = new Scene(v, img.getWidth(), img.getHeight());
 			scene.setFill(null);
 			
-			Image img = new Image("GUI/CookingBookScantitreSD.jpg");
-			ImageView imgView = new ImageView(img);
+			//imgView.setFitHeight(352);
+			//imgView.setFitWidth();
 			v.getChildren().add(imgView);
 			
 			primaryStage.centerOnScreen();
-			//primaryStage.setFullScreen(true);
+			primaryStage.setFullScreen(true);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
