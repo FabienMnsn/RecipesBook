9d5d9d47ea57ef3a58e874e5dc100a281d5ea601
FabienManson <fabien.manson@free.fr>
2019-07-07 16:53:10 +0200

    adding main menu button w8

--- RecipesBook/src/GUI/BookWindowTest.java
+++ RecipesBook/src/GUI/BookWindowTest.java
@@ -3,11 +3,13 @@ package GUI;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
@@ -21,8 +23,28 @@ public class BookWindowTest extends Application {
 		try {
 			//BorderPane root = new BorderPane();
 			//Scene scene = new Scene(root, 400, 400);
-			VBox v = new VBox();
-			v.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
+			Button readB = new Button("Consulter");
+			Button editB = new Button("Modifier");
+			editB.setTranslateY(65);
+			readB.setStyle(""
+					+ "-fx-font: 25 arial; "
+					+ "-fx-base: #660000; "
+					+ "-fx-border-color: #b30000; "
+					+ "-fx-text-fill: #808080; "
+					+ "-fx-border-width: 1px; "
+					+ "-fx-border_style: solid;");
+			
+			editB.setStyle(""
+					+ "-fx-font: 25 arial; "
+					+ "-fx-base: #660000; "
+					+ "-fx-border-color: #b30000; "
+					+ "-fx-text-fill: #808080; "
+					+ "-fx-border-width: 1px; "
+					+ "-fx-border_style: solid;");
+			
+			StackPane stackpane = new StackPane();
+			//VBox v = new VBox();
+			//v.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
 			//v.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, null, null)));
 			
 			Rectangle2D screen = Screen.getPrimary().getVisualBounds();
@@ -31,16 +53,17 @@ public class BookWindowTest extends Application {
 			ImageView imgView = new ImageView(img);
 			System.out.println("Image resize ["+img.getHeight()+", "+img.getWidth()+"]");
 			
-			Scene scene = new Scene(v, img.getWidth(), img.getHeight());
+			Scene scene = new Scene(stackpane, img.getWidth(), img.getHeight());
 			scene.setFill(null);
 			
 			//imgView.setFitHeight(352);
 			//imgView.setFitWidth();
-			v.getChildren().add(imgView);
+			stackpane.getChildren().addAll(imgView, readB, editB);
 			
 			primaryStage.centerOnScreen();
-			primaryStage.setFullScreen(true);
-			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			//primaryStage.setFullScreen(true);
+			//primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.initStyle(StageStyle.DECORATED);
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
